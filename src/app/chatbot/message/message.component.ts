ec8acb8e5b2884b28249401b76e7cbfb7ea1cff7
VeronikaMi <nika.mitichashili@gmail.com>
2022-05-21 14:40:18 +0400

    started

---
+++ src/app/chatbot/message/message.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'message',
+  templateUrl: './message.component.html',
+  styleUrls: ['./message.component.css'],
+})
+export class MessageComponent {
+  @Input('text') text: string;
+  @Input('date') date: any;
+  @Input('owner') owner: boolean;
+  @Input('colorBackRight') colorBackRight: string;
+  @Input('colorFontRight') colorFontRight: string;
+  @Input('colorBackLeft') colorBackLeft: string;
+  @Input('colorFontLeft') colorFontLeft: string;
+}
