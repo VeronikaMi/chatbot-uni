7e5a26cbad4f4c07e79e860a265f9fce92940c49
VeronikaMi <nika.mitichashili@gmail.com>
2022-05-25 14:48:09 +0400

    feat: animations + collapse

--- src/app/chatbot.service.ts
+++ src/app/chatbot.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-// import { Headers, Http, RequestOptions } from '@angular/http';
+// import { Headers, Http, RequestOptions } from '@angular/common/http';
 // import { Observable } from 'rxjs/Observable';
 // import 'rxjs/add/operator/map';
 // import 'rxjs/add/operator/toPromise';
--- src/app/chatbot/chatbot.component.ts
+++ src/app/chatbot/chatbot.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { Component, Input, OnInit } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { ChatbotService } from '../chatbot.service';
 
 @Component({
@@ -6,13 +8,13 @@ import { ChatbotService } from '../chatbot.service';
   templateUrl: './chatbot.component.html',
   styleUrls: ['./chatbot.component.scss'],
 })
-export class ChatbotComponent {
+export class ChatbotComponent implements OnInit {
   BACK_ENABLED: boolean = true;
 
   @Input('messages') messages: any[] = [
     {
       text: 'Lark has a friendly, kind and humorous persona that appeals to seniors, its largest clientele. Users can engage with the chatbot through chat, voice and button options.',
-      date: new Date().getDate(),
+      date: this.getTime(),
       userOwner: false,
     },
   ];
@@ -22,6 +24,10 @@ export class ChatbotComponent {
   @Input('colorFontLeft') colorFontLeft: string;
 
   textInput: string = '';
+  public showChatbot: boolean = false;
+  // public showChatbotChange: BehaviorSubject<boolean> =
+  //   new BehaviorSubject<boolean>(false);
+  public timestamp: string;
 
   constructor(private chatService: ChatbotService) {}
 
@@ -30,28 +36,52 @@ export class ChatbotComponent {
     this.colorFontRight ? this.colorFontRight : '#ffffff';
     this.colorBackLeft ? this.colorBackLeft : '#f8f9fa';
     this.colorFontLeft ? this.colorFontLeft : '#343a40';
+
+    // this.showChatbotChange.subscribe((state) => {
+    //   this.showChatbot = state;
+    //   if (this.showChatbot) {
+    //     this.timestamp = this.getTime();
+    //   }
+    // });
   }
 
   sendMessage() {
-    let newMessage = { text: this.textInput, date: '', userOwner: true };
-    this.messages.push(newMessage);
-    let messageBack = { firstname: 'Simon', text: this.textInput };
-    // if (this.BACK_ENABLED) {
-    //   this.chatService.sendMessage(messageBack).then((res) => {
-    //     let messageReturn = {
-    //       text: res.json().speech_answer,
-    //       date: '',
-    //       userOwner: false,
-    //     };
-    //     this.messages.push(messageReturn);
-    //   });
-    // }
-    this.textInput = '';
+    if (this.textInput.length > 0) {
+      let newMessage = { text: this.textInput, date: '', userOwner: true };
+      this.messages.push(newMessage);
+      let messageBack = { firstname: 'Simon', text: this.textInput };
+      // if (this.BACK_ENABLED) {
+      //   this.chatService.sendMessage(messageBack).then((res) => {
+      //     let messageReturn = {
+      //       text: res.json().speech_answer,
+      //       date: '',
+      //       userOwner: false,
+      //     };
+      //     this.messages.push(messageReturn);
+      //   });
+      // }
+      this.textInput = '';
+    }
+  }
+
+  public showChat() {
+    this.showChatbot = !this.showChatbot;
+    if (!this.timestamp) {
+      this.timestamp = this.getTime();
+    }
   }
 
   onKey(event: any) {
     if (event.keyCode == 13) {
       this.sendMessage();
     }
+  }
+
+  private getTime(): string {
+    const date = new Date();
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+
+    return `${hours} : ${minutes}`;
   }
 }
