e738764173ac141703795a76065d3e6519051b8f
VeronikaMi <nika.mitichashili@gmail.com>
2022-06-20 12:56:00 +0400

    fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,11 @@ import { ContactPageComponent } from './pages/contact/contact.component';
 import { HomePageComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomePageComponent },
+  // { path: 'home', component: HomePageComponent },
+  { path: '', component: HomePageComponent, pathMatch: 'full' },
   { path: 'contact', component: ContactPageComponent },
   { path: 'about', component: AboutPageComponent },
-  { path: '**', redirectTo: 'home' },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
